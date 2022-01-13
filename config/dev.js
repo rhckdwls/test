432b81bf9af5dd658d2a69b5ac84a6356bc19dd2
rhckdwls <92511809+rhckdwls@users.noreply.github.com>
2022-01-13 15:29:59 +0900

    Add files via upload

---
+++ config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://rhckdwls12:baram1@cluster0.tvewf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+};
---
+++ precache-manifest.052d6885d0d203056bb2adf94c7d2f85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aff3240d380c6b63b80de50539b33f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec631ab4ac7d61b14069",
+    "url": "/static/css/2.88c18c32.chunk.css"
+  },
+  {
+    "revision": "4efa42ae3c6140224473",
+    "url": "/static/css/main.6b3f3534.chunk.css"
+  },
+  {
+    "revision": "ec631ab4ac7d61b14069",
+    "url": "/static/js/2.d9accdd2.chunk.js"
+  },
+  {
+    "revision": "4f160e29840e1f64ec2468f8c9029bf3",
+    "url": "/static/js/2.d9accdd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4efa42ae3c6140224473",
+    "url": "/static/js/main.b99c33d1.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  }
+]);
